3077c63a7eb83e5887ab5704b05cc4fac0a9ac7b
Abdulla <syedabdullawaseem635@gmail.com>
2022-10-29 12:01:48 -0700

    Hello world

---
+++ GitTutorial/Demo.js
@@ -0,0 +1,5 @@
+
+
+var headerTitle=document.getElementById('header-title');
+headerTitle.style.borderBottom='solid 3px #000';
+console.log(headerTitle)
---
+++ task4.js
@@ -0,0 +1,7 @@
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px black'
+var items=document.getElementsByClassName('list-group-item')
+items[2].style.backGroungcolor='green';
+items.style.fontweight='bold';
+<script src="task4.html"></script>
