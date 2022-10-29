6fcbd8f467599a0577d70dff4f467daead41c62b
Abdulla <syedabdullawaseem635@gmail.com>
2022-10-28 22:01:59 -0700

    adding third file

---
+++ main.js
@@ -0,0 +1,6 @@
+let ul=document.querySelector('.items');
+ul.firstElementChild.textContent='Click';
+ul.children[1].innerText='mouseover';
+ul.lastElementChild.innerHTML='mouseout';
+const btn=document.querySelector('.btn');
+btn.getElementsByClassName.background='red';
